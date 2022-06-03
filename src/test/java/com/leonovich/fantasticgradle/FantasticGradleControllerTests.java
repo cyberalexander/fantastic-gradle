2a8d289bf124bd43458c7c20696ab1038b348648
CyberAlexander <taurum13@gmail.com>
2022-06-03 09:15:49 +0300

    Unused import removed

--- src/test/java/com/leonovich/fantasticgradle/FantasticGradleControllerTests.java
+++ src/test/java/com/leonovich/fantasticgradle/FantasticGradleControllerTests.java
@@ -10,7 +10,6 @@ import org.assertj.core.api.Assertions;
 import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
