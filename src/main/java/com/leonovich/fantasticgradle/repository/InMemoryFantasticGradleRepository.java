d7a71ca60cacbc6e8bb7e3e3f1ccfb4b8cfeb52c
CyberAlexander <taurum13@gmail.com>
2022-06-07 07:43:47 +0300

    Get method in Repository associated with in-memory storage

--- src/main/java/com/leonovich/fantasticgradle/repository/InMemoryFantasticGradleRepository.java
+++ src/main/java/com/leonovich/fantasticgradle/repository/InMemoryFantasticGradleRepository.java
@@ -16,11 +16,9 @@
 package com.leonovich.fantasticgradle.repository;
 
 import com.leonovich.fantasticgradle.model.FantasticGradle;
-import org.jeasy.random.EasyRandom;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -29,19 +27,12 @@ import java.util.UUID;
 @Primary
 @Component
 public class InMemoryFantasticGradleRepository implements FantasticRepository<FantasticGradle, UUID> {
-    private static final EasyRandom EASY_RANDOM = new EasyRandom();
 
     private static final Map<UUID, FantasticGradle> STORAGE = new HashMap<>();
 
     @Override
     public Optional<FantasticGradle> get(UUID fantasticGradleId) {
-        return Optional.ofNullable(
-                FantasticGradle.builder()
-                        .fantasticGradleId(fantasticGradleId)
-                        .name(EASY_RANDOM.nextObject(String.class))
-                        .createdWhen(LocalDateTime.now())
-                        .build()
-        );
+        return Optional.ofNullable(STORAGE.get(fantasticGradleId));
     }
 
     @Override
