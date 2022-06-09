789cb6055a198fd968e0140c0e86526477d3de21
CyberAlexander <taurum13@gmail.com>
2022-06-09 08:43:40 +0300

    FantasticGradleV2Controller GET operation modified to retrieve the data from in-memory storage

--- src/main/java/com/leonovich/fantasticgradle/controller/FantasticGradleV2Controller.java
+++ src/main/java/com/leonovich/fantasticgradle/controller/FantasticGradleV2Controller.java
@@ -16,13 +16,16 @@
 package com.leonovich.fantasticgradle.controller;
 
 import com.leonovich.fantasticgradle.dto.FantasticGradleDto;
+import com.leonovich.fantasticgradle.mapper.FantasticGradleModelMapper;
+import com.leonovich.fantasticgradle.model.FantasticGradle;
+import com.leonovich.fantasticgradle.repository.FantasticRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -34,17 +37,25 @@ import java.util.UUID;
 public class FantasticGradleV2Controller {
     private static final String API_VERSION_SUFFIX = "_V2";
 
+    private FantasticGradleModelMapper mapper;
+
+    private FantasticRepository<FantasticGradle, UUID> repository;
+
     /**
      * Example of UUID : 4fcfc816-2f2f-420a-b159-4150366e489c or use {@code UUID.randomUUID()}
+     *
      * @param fantasticGradleId The unique identifier of fantastic gradle
      * @return The instance of {@link FantasticGradleDto} associated with given {@param fantasticGradleId}
      */
     @GetMapping(path = "{fantasticGradleId}")
     public FantasticGradleDto getFantasticGradle(@PathVariable UUID fantasticGradleId) {
-        return FantasticGradleDto.builder()
-                .fantasticGradleId(fantasticGradleId)
-                .name("FantasticGradle".concat(API_VERSION_SUFFIX))
-                .createdWhen(LocalDateTime.now())
-                .build();
+        Optional<FantasticGradle> fantasticGradle = repository.get(fantasticGradleId);
+        return fantasticGradle
+                .map(result -> {
+                    result.setName(result.getName().concat(API_VERSION_SUFFIX));
+                    return result;
+                })
+                .map(result -> mapper.map(result))
+                .orElse(null);
     }
 }
