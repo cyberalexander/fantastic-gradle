95d484f6500d0f1d9e710c83ac9dfaffa1d81b84
CyberAlexander <taurum13@gmail.com>
2022-08-31 09:59:38 +0300

    Configuring Open API Documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,8 +117,7 @@ dependencies {
 	// Added during development
 	compileOnly("javax.validation:validation-api")
 	implementation("org.mapstruct:mapstruct:1.5.2.Final")
-	implementation("io.springfox:springfox-swagger2:3.0.0")
-	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
 	/**
 	 * Actually it's kinda tool to be used in unit-tests to generate test-data. But I use it in application code.
 	 */
---
+++ src/main/java/com/leonovich/fantasticgradle/configuration/OpenApiConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Aliaksandr Leanovich. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.leonovich.fantasticgradle.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a href="https://swagger.io/">swagger documentation</a>
+ *
+ * Created : 30/08/2022 09:51
+ * Project : fantastic-gradle
+ * IDE : IntelliJ IDEA
+ *
+ * @author Aliaksandr_Leanovich
+ * @version 1.0
+ */
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Fantastic Gradle API")
+                        .description("Fantastic gradle Open API Documentation")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache License 2.0").url("http://springdoc.org"))
+                        .description("SpringShop Wiki Documentation")
+                        .contact(
+                                new Contact().email("test@test.com")
+                                        .url("https://github.com/cyberalexander/fantastic-gradle")
+                        )
+                );
+    }
+}
